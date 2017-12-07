91a33fc18cd970f334b3b905bb0de55bec0b8643
rainer.telesko <rainer.telesko@OL19AM00272.adm.ds.fhnw.ch>
2017-12-07 10:24:28 +0100

    First commit

--- ReqEng_AS_17/src/exercises/GCDTest.java
+++ ReqEng_AS_17/src/exercises/GCDTest.java
@@ -15,5 +15,9 @@ public class GCDTest {
 		assertEquals("9 is the GCD", 9, GCD.greatestCommonDivisor(27, 9));
 	}
 	
+	@Test
+	public void testGCD3() {
+		assertEquals("3 is the GCD", 3, GCD.greatestCommonDivisor(9, 6));
+	}
 	
 }
